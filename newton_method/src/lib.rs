eb64da6d09e87839bac4ba0efcf8e5317c9502a8
Undercaffinated <nathan.c.howell@pm.me>
2026-08-28 18:22:33 -0500

    Clean notation for newton_iteration method. Improve related docs.

--- newton_method/src/lib.rs
+++ newton_method/src/lib.rs
@@ -8,22 +8,23 @@ pub fn newton_method(
 }
 
 #[allow(unused)]
-fn newton_iteration(initial: f64, function: fn(f64) -> f64, derivative: fn(f64) -> f64) -> f64 {
+fn newton_iteration(x0: f64, f: fn(f64) -> f64, dfdx: fn(f64) -> f64) -> f64 {
+    let f_x0: f64 = f(x0);
+    let dfdx_x0: f64 = dfdx(x0);
     /*
-        Step 3: Define the line tangent to f that intersects (initial, f(initial)).
-        Step 4: Find the x-intercept of that line
-        Step 5: Decide if we're done or need to iterate again.
-    */
+        Point-Slope of a Line := y-f(x0) = m(x-x0)
 
-    let f: f64 = function(initial);
-    let f_prime: f64 = derivative(initial);
-    /*
-        From y = f'(x_0)(x-x_0) + f(x_0), since we care about the x-intercept,
-        we can set y = 0.
-        Thus, f'(x_0)(x-x_0) + f(x_0) = 0 becomes
-        x = (x_0 * f'(x_0) - f(x_0)) / f'(x_0)
+        Since we want to define this line to be parallel to the tangent line at (x0, f(x0)), we define
+        m∈ℝ = f'(x0), resulting in y-f(x0) = f'(x0)*(x-x0).
+
+        We add f(x0) to both sides, resulting in y = f'(x0)*(x-x0) + f(x0)
+
+        From this, since we care about the x-intercept we can set y = 0.
+        Thus, f'(x0)*(x-x0) + f(x0) = 0.
+
+        Solving for x, we get x = (x0 * f'(x0) - f(x_0)) / f'(x_0)
     */
-    (initial * derivative(initial) - function(initial)) / derivative(initial)
+    (x0 * dfdx_x0 - f_x0) / dfdx_x0
 }
 
 #[cfg(test)]
@@ -35,6 +36,8 @@ mod tests {
         let initial: f64 = 2.0;
         let x_1: f64 =
             newton_iteration(initial, _quadratic_function, _quadratic_function_derivative);
+        // Here we note 0.0 is the actual x-intercept. For the expression f(x) = x^2,
+        // newton_method(n -> inf, x^2, 2x) == 0.0
         assert!(0.0 < x_1 && x_1 < initial);
     }
 }
